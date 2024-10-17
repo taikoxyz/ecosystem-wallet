3ba47e67426169f284cc82b92db7c2be5eb9ce56
Jaume Alavedra <jamalavedra@gmail.com>
2024-10-17 20:22:11 +0200

    chore: update version

--- ecosystem-sdk/sdk-sample/app/utils/ecosystemWallet.ts
+++ ecosystem-sdk/sdk-sample/app/utils/ecosystemWallet.ts
@@ -1,8 +1,10 @@
 import EcosystemWallet from '@rapidfire/id';
 
 // create the Ecosystem instance and export it so it can be used in the examples
+const domain = typeof window !== 'undefined' ? window.location.origin : process.env.NEXT_PUBLIC_DOMAIN || "http://localhost:3004";
+console.log('domain', domain)
 export const ecosystemWalletInstance = new EcosystemWallet({
   clientId: process.env.NEXT_PUBLIC_CLIENT_ID!,
-  redirectUri: "http://localhost:5173/redirect",
-  logoutRedirectUri: "http://localhost:5173/logout",
+  redirectUri: `${domain}/redirect`,
+  logoutRedirectUri: `${domain}/logout`,
 });
