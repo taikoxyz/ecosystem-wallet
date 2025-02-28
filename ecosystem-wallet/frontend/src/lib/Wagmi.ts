2a56af49c60bb82d3335d2fc7c0dc423239dd420
Jaume Alavedra <jamalavedra@gmail.com>
2025-02-28 16:09:11 +0100

    chore: add support for ancient8 sepolia

--- ecosystem-wallet/frontend/src/lib/Wagmi.ts
+++ ecosystem-wallet/frontend/src/lib/Wagmi.ts
@@ -1,12 +1,13 @@
 import { http, createConfig } from 'wagmi'
-import { baseSepolia, polygonAmoy, sepolia } from 'wagmi/chains'
+import { ancient8Sepolia, baseSepolia, polygonAmoy, sepolia } from 'wagmi/chains'
 
 export const config = createConfig({
-  chains: [polygonAmoy, baseSepolia, sepolia],
+  chains: [polygonAmoy, baseSepolia, sepolia, ancient8Sepolia],
   transports: {
     [polygonAmoy.id]: http(),
     [baseSepolia.id]: http("https://newest-radial-gadget.base-sepolia.quiknode.pro/a33177b3c598ebf17b67f1f0f3d4c4f2d7c04913"),
     [sepolia.id]: http(),
+    [ancient8Sepolia.id]: http(),
   },
 })
 
