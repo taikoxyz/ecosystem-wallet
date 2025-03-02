0bbf07c6d4967e5ab2f55db66738077980838c1d
Jaume Alavedra <jamalavedra@gmail.com>
2025-03-02 23:54:37 +0100

    chore: update ecosystem-js

--- ecosystem-sdk/sdk-sample/next.config.mjs
+++ ecosystem-sdk/sdk-sample/next.config.mjs
@@ -4,6 +4,19 @@ const nextConfig = {
         config.resolve.fallback = { fs: false, net: false, tls: false };
         return config;
     },
-}   
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Cross-Origin-Opener-Policy',
+                        value: 'same-origin-allow-popups'
+                    }
+                ]
+            }
+        ];
+    }
+}
 
 export default nextConfig;
