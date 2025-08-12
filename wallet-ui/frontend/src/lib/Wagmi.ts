0d860b1e019bf5bba43ca930793063e1a81fa150
Jaume Alavedra <jamalavedra@gmail.com>
2025-08-12 16:30:39 +0200

    chore: add fallback rapidfire

--- wallet-ui/frontend/src/lib/Wagmi.ts
+++ wallet-ui/frontend/src/lib/Wagmi.ts
@@ -1,11 +1,16 @@
-import { http, createConfig } from 'wagmi'
+import { http, createConfig, fallback } from 'wagmi'
 import { ancient8Sepolia, baseSepolia, polygonAmoy, sepolia, dosChainTestnet } from 'wagmi/chains'
 
 export const config = createConfig({
   chains: [polygonAmoy, baseSepolia, sepolia, ancient8Sepolia, dosChainTestnet],
   transports: {
     [polygonAmoy.id]: http('https://polygon-amoy.gateway.tenderly.co'),
-    [baseSepolia.id]: http("https://newest-radial-gadget.base-sepolia.quiknode.pro/a33177b3c598ebf17b67f1f0f3d4c4f2d7c04913"),
+    [baseSepolia.id]: fallback([
+      http("https://newest-radial-gadget.base-sepolia.quiknode.pro/a33177b3c598ebf17b67f1f0f3d4c4f2d7c04913"),
+      http("https://base-sepolia-rpc.publicnode.com"),
+      http("https://sepolia.base.org"),
+      http("https://base-sepolia.drpc.org"),
+    ]),
     [sepolia.id]: http("https://ethereum-sepolia-rpc.publicnode.com"),
     [ancient8Sepolia.id]: http(),
     [dosChainTestnet.id]: http(),
